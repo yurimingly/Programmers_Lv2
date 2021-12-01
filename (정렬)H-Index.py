7b70c2e7ca172798eaf7a6c96e85282a0cb8b9ad
yurimingly <yuyu9895@likelion.org>
2021-12-01 19:12:05 +0900

    21-12-01 정렬 완성
    lv2는 개껌이제

---
+++ (정렬)H-Index.py
@@ -0,0 +1,55 @@
+https://programmers.co.kr/learn/courses/30/lessons/42747
+
+'''
+H-Index
+dark
+light
+sublime
+vim
+emacs
+Python3 
+문제 설명
+H-Index는 과학자의 생산성과 영향력을 나타내는 지표입니다. 어느 과학자의 H-Index를 나타내는 값인 h를 구하려고 합니다. 위키백과1에 따르면, H-Index는 다음과 같이 구합니다.
+
+어떤 과학자가 발표한 논문 n편 중, h번 이상 인용된 논문이 h편 이상이고 나머지 논문이 h번 이하 인용되었다면 h의 최댓값이 이 과학자의 H-Index입니다.
+
+어떤 과학자가 발표한 논문의 인용 횟수를 담은 배열 citations가 매개변수로 주어질 때, 이 과학자의 H-Index를 return 하도록 solution 함수를 작성해주세요.
+
+제한사항
+과학자가 발표한 논문의 수는 1편 이상 1,000편 이하입니다.
+논문별 인용 횟수는 0회 이상 10,000회 이하입니다.
+입출력 예
+citations	return
+[3, 0, 6, 1, 5]	3
+입출력 예 설명
+이 과학자가 발표한 논문의 수는 5편이고, 그중 3편의 논문은 3회 이상 인용되었습니다. 그리고 나머지 2편의 논문은 3회 이하 인용되었기 때문에 이 과학자의 H-Index는 3입니다.
+
+※ 공지 - 2019년 2월 28일 테스트 케이스가 추가되었습니다.
+'''
+
+#1
+def solution(citations):
+    answer = 0
+    citations.sort(reverse=True)
+    tmp = []
+    
+    for i in citations: # 전체 순환
+        cnt = 0
+        
+        for k in citations:   # i를 포함한 나머지 배열 순환할때
+            if i <= k:        # 나머지배열의 k인용횟수가 >= 해당 i번의 인용횟수보다 크거나 같으면
+                cnt += 1      # i번째 인용횟수보다 인용을 했구나!!
+            else : break      # 그렇지 않으면 break(sort를 했기 때문에 그 다음은 이보다 작을것임)
+                
+        if cnt <= i:          # h번 이상 인용된 논문이 h편 이상이고
+            tmp.append(cnt)
+            
+    if sum(citations) == 0 : return 0   ########[0,0,0] 테케 방지     
+    tmp.sort(reverse=True) #나머지 논문이 h번 이하 인용되었다면
+    return tmp[0] #h의 최댓값이 이 과학자의 H-Index입니다.
+
+#2
+def solution(citations):
+    citations.sort(reverse=True)
+    answer = max(map(min, enumerate(citations, start=1)))
+    return answer
