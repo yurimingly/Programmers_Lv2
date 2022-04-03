21274ee9d922c5c30deaa5886a7da72b79f21046
yurimingly <yuyu9895@likelion.org>
2022-04-04 00:29:38 +0900

    20220404 lv2 프로그래머스

---
+++ (greedy)조이스틱.py
@@ -0,0 +1,57 @@
+https://programmers.co.kr/learn/courses/30/lessons/42860
+
+'''
+조이스틱
+
+darklight
+
+sublimevimemacs
+
+문제 설명
+
+조이스틱으로 알파벳 이름을 완성하세요. 맨 처음엔 A로만 이루어져 있습니다.
+
+ex) 완성해야 하는 이름이 세 글자면 AAA, 네 글자면 AAAA
+
+조이스틱을 각 방향으로 움직이면 아래와 같습니다.
+
+▲ - 다음 알파벳 ▼ - 이전 알파벳 (A에서 아래쪽으로 이동하면 Z로) ◀ - 커서를 왼쪽으로 이동 (첫 번째 위치에서 왼쪽으로 이동하면 마지막 문자에 커서) ▶ - 커서를 오른쪽으로 이동 (마지막 위치에서 오른쪽으로 이동하면 첫 번째 문자에 커서) 
+
+예를 들어 아래의 방법으로 "JAZ"를 만들 수 있습니다.
+
+- 첫 번째 위치에서 조이스틱을 위로 9번 조작하여 J를 완성합니다. - 조이스틱을 왼쪽으로 1번 조작하여 커서를 마지막 문자 위치로 이동시킵니다. - 마지막 위치에서 조이스틱을 아래로 1번 조작하여 Z를 완성합니다. 따라서 11번 이동시켜 "JAZ"를 만들 수 있고, 이때가 최소 이동입니다. 
+
+만들고자 하는 이름 name이 매개변수로 주어질 때, 이름에 대해 조이스틱 조작 횟수의 최솟값을 return 하도록 solution 함수를 만드세요.
+
+제한 사항
+
+name은 알파벳 대문자로만 이루어져 있습니다.
+
+name의 길이는 1 이상 20 이하입니다.
+
+입출력 예
+'''
+
+def solution(name):
+
+	# 조이스틱 조작 횟수 
+    answer = 0
+    
+    # 기본 최소 좌우이동 횟수는 길이 - 1
+    min_move = len(name) - 1
+    
+    for i, char in enumerate(name):
+    	# 해당 알파벳 변경 최솟값 추가
+        answer += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
+        
+        # 해당 알파벳 다음부터 연속된 A 문자열 찾기
+        next = i + 1
+        while next < len(name) and name[next] == 'A':
+            next += 1
+            
+        # 기존, 연속된 A의 왼쪽시작 방식, 연속된 A의 오른쪽시작 방식 비교 및 갱신
+        min_move = min([min_move, 2 *i + len(name) - next, i + 2 * (len(name) -next)])
+        
+    # 알파벳 변경(상하이동) 횟수에 좌우이동 횟수 추가
+    answer += min_move
+    return answer
